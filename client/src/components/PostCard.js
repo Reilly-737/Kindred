34d5332e1f889552189328380f5d76768a009b1e
Reilly <reillyswork@gmail.com>
2023-12-15 00:34:00 -0700

    comment section started

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -3,19 +3,13 @@ import { Link } from "react-router-dom";
 import CommentSection from "./CommentSection";
 import "./PostCard.css";
 
-const PostCard = ({
-  title,
-  id,
-  tags,
-  body,
-  username,
-  user,
-}) => {
+const PostCard = ({ title, id, tags, body, username, user }) => {
+  console.log("Post ID:", id);
   return (
     <div className="post-card-container">
+      <div className="post-username">Artist: {username}</div>
       <Link to={`/posts/${id}`} className="post-link">
         <div className="post-card">
-          <div className="post-username">Artist: {username}</div>
           <div className="post-details">
             <h2>{title}</h2>
             <div className="post-tags">
@@ -25,7 +19,7 @@ const PostCard = ({
           </div>
         </div>
       </Link>
-      <CommentSection postId={id} user={user}  />
+      <CommentSection postId={id} user={user} />
     </div>
   );
 };
